1c8569775c1e61aa0d16540eb9d9424949d39270
KauaRohling <rohling.fernandes@escola.pr.gov.br>
2025-07-03 08:28:16 -0300

    ok

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ function inserirNome(){
  elemento.textContent = nomeUsuario;
 }
 inserirNome();
-let linguagens = ["JavaScript", "Python","C++"]
-
-console.log (linguagens [0])
-
-console.log (linguagens [1])
-
-console.log (linguagens [2])
-const lista = document.querySelector ("#lista");
-
-lista.textContent = linguagens [0];
-
-lista.textContent = linguagens [1];
-
-lista.textContent = linguagens [2];
